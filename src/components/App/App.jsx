ddab2956a21d9f94f3ca06ff599e110e04295bfa
Ivaskiv <viraivaskiv@gmail.com>
2024-02-21 10:30:10 +0200

    upd app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -27,9 +27,7 @@ function App() {
       {filteredContacts.length !== 0 ? (
         <ContactList contacts={filteredContacts} onDelete={handleDeleteContact} />
       ) : (
-        <p className="searchText">
-          There are no contacts in your phonebook. Please add your first contact!
-        </p>
+        <p className="noContactsText">There are no contacts in your phonebook!</p>
       )}
     </div>
   );
